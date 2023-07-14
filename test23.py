a98b36815bba487901945445bbc1994c3857b916
MananSharma134 <data26manan@gmail.com>
2023-07-14 11:26:04 +0530

    This is a Change a value in Code

--- test23.py
+++ test23.py
@@ -1,1 +1,10 @@
-print("This is a task ")
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# Example usage
+number = 5
+fact = factorial(number)
+print(f"The factorial of {number} is: {fact}")
